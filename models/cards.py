f748e693dfa422d1264a70001cff3435c3f588e4
Josip Dorvak <jdorvak@gmail.com>
2024-07-23 15:17:24 -0400

    Add scryfall search function

--- models/cards.py
+++ models/cards.py
@@ -1,4 +1,9 @@
+import requests
+
+
 class Card:
+    SCRYFALL_API_URL = "https://api.scryfall.com/cards/search"
+
     def __init__(self, name, set_name, set_code, rarity, collection_number, card_type, image_url):
         self.name = name
         self.set_name = set_name
@@ -13,7 +18,17 @@ class Card:
                 f"Set Name: {self.set_name}\n"
                 f"Card Type: {self.card_type}")
 
+    @staticmethod
+    def search_card_scryfall(name, set_code, collection_number):
+        response = requests.get(f"{Card.SCRYFALL_API_URL}?&q=!{name.replace(" ", "")}+set%3a{set_code}+cn%3A{collection_number}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Error")
+
 
 if __name__ == "__main__":
     test_card = Card(name="testing", set_name="Test Set", set_code="TST", rarity="Mythic", collection_number=000, card_type="Test", image_url=None)
     print(test_card)
+
+    Card.search_card_scryfall("sol ring", "pip", "359")
