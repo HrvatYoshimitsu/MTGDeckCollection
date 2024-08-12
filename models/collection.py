faf8dc840bc03e70040a121af0372c462642c6e1
Josip Dorvak <jdorvak@gmail.com>
2024-08-12 16:06:47 -0400

    change type to card_type

--- models/collection.py
+++ models/collection.py
@@ -14,9 +14,9 @@ class Collection:
     def remove_deck(self):
         pass
 
-    def add_card(self, name, set_name, set_code, rarity, col_num, type):
+    def add_card(self, name, set_name, set_code, rarity, col_num, card_type):
         new_card = Card(name=name, set_name=set_name, set_code=set_code, rarity=rarity, collection_number=col_num,
-                        card_type=type, image_url=None)
+                        card_type=card_type, image_url=None)
         self.cards.append(new_card)
 
     def remove_card(self):
